6e24156effe5730b566382d39804086772de43c1
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 08:45:29 +0800

    styles(config): :sparkles: add extend classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        default: "Poppins, sans-serif",
+      },
+      colors: {
+        "gold-accent": "#D29000",
+      },
+      boxShadow: {
+        card: "0px 14px 85px rgba(176, 125, 14, 0.14), 0px 4.22059px 25.625px rgba(176, 125, 14, 0.0912224), 0px 1.75302px 10.6433px rgba(176, 125, 14, 0.07), 0px 0.634032px 3.84948px rgba(176, 125, 14, 0.0487776)",
+      },
+    },
+  },
 };
