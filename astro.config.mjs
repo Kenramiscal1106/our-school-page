12dfa7eb3fb835df8e7e6a9f2a476a4272cc389c
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-01 08:45:02 +0800

    styles(config): :bug: fix apply base styles by setting it to false

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
-import { defineConfig } from 'astro/config';
-import tailwindIntegration from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import tailwindIntegration from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 5007
+    port: 5007,
   },
-  integrations: [tailwindIntegration()]
+  integrations: [
+    tailwindIntegration({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+  ],
 });
