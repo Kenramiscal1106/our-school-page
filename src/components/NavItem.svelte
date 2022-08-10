df3cd55a819aa2db4a2b4d306af27cdcccf3d5c3
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-08-11 07:03:23 +0800

    refactor(components): :sparkles: add svelte files

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let text;
+  export let dropdown;
+  export let href;
+  export let isScrolled;
+</script>
+
+{#if dropdown}
+  <button class="p-4">{text}</button>
+  <div class="p-4">
+    <slot />
+  </div>
+{:else}
+  <a {href} class={`p-4 ${isScrolled ? "text-white" : ""}`}>{text}</a>
+{/if}
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,57 @@
+<script>
+  import { onMount } from "svelte";
+  let isScrolled = false;
+  import NavItem from "./NavItem.svelte";
+  import logo from "../assets/logo.svg";
+  onMount(() => {
+    if (window.scrollY > 0) {
+      isScrolled = true;
+    }
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        isScrolled = true;
+        return;
+      }
+      isScrolled = false;
+    });
+  });
+</script>
+
+<nav
+  class={`transition-[background-color] duration-200 ${
+    isScrolled
+      ? "fixed bg-secondary text-white slide-in-down"
+      : "bg-transparent absolute"
+  }  top-0 left-0 w-full flex`}
+>
+  <div class="flex-1 flex items-center justify-center">
+    <a href="/" class="text-2xl  flex items-center">
+      <img src={logo} alt="logo" class="h-8" />
+      <span class="text-3xl font-bold">LCCT</span>
+    </a>
+  </div>
+  <div class="flex-1 flex">
+    <NavItem text="Academic programs" href="/academic-programs" {isScrolled} />
+    <NavItem
+      text="Events & activities"
+      href="/events-activities"
+      {isScrolled}
+    />
+    <NavItem text="Contacts" href="/contacts" {isScrolled} />
+  </div>
+  <div class="flex-1" />
+</nav>
+
+<style>
+  @keyframes slide-in-down {
+    from {
+      transform: translateY(-100%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  .slide-in-down {
+    animation: slide-in-down 0.3s ease-in-out;
+  }
+</style>
