995bc4403994071ff815785ed54283d04611e2f8
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-11-04 11:12:25 +0800

    feat: :sparkles: add home navbar

---
+++ src/components/HomeNavbar.svelte
@@ -0,0 +1,56 @@
+<script>
+  import { onMount } from "svelte";
+  let isScrolled = false;
+  import NavItem from "./NavItem.svelte";
+  import logo from "../assets/logo.svg";
+  /* onMount(() => {
+    if (window.scrollY > 0) {
+      isScrolled = true;
+    }
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        isScrolled = true;
+        return;
+      }
+      isScrolled = false;
+    });
+  }); */
+</script>
+
+<!-- /* `transition-[background-color] duration-200 ${
+    isScrolled
+      ? "fixed bg-secondary text-white slide-in-down"
+      : "bg-transparent absolute"
+  }  top-0 left-0 w-full` */ -->
+<nav class="fixed items-center top-0 text-black py-4 px-3 flex w-full">
+  <div class="mr-auto">
+    <div class="p-3 flex items-center">
+      <a href="/" class="text-2xl gap-1 flex items-center">
+        <img src={logo} alt="logo" class="h-8" />
+        <span class="text-3xl font-bold">LCCT</span>
+      </a>
+    </div>
+  </div>
+  <div>
+    <div class="flex">
+      <NavItem href="/academic-programs" text="Academics" />
+      <NavItem href="/events-activities" text="Events & activites" />
+      <NavItem href="/contacts" text="Contacts" />
+      <NavItem href="https://preregis.lcctanauan.edu.ph/" text="Preregis" />
+    </div>
+  </div>
+</nav>
+
+<style>
+  @keyframes slide-in-down {
+    from {
+      transform: translateY(-100%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  .slide-in-down {
+    animation: slide-in-down 0.3s ease-in-out;
+  }
+</style>
