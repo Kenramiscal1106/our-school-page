a94bd9876ad4ed9fda6d350443ba80b0fdaf9c82
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2022-11-04 15:51:38 +0800

    refactor(config): :fire: remove some comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwindIntegration from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
   server: {
     port: 5007,
