0fd2e0d840939245327f62e9c948b0bb7b2e8c21
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-04-03 03:19:25 +0800

    feat(components): :label: :bug: migrate to ts; fix bug

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import NavItem from "./NavItem.svelte";
   import logo from "../assets/logo.svg";
   import hBIcon from "../assets/bar-menu.svg";
@@ -18,21 +18,29 @@
 
 <nav class="fixed items-center top-0 text-black flex w-full">
   <div class="mr-auto p-3 flex items-center">
-    <a href="/" class="text-2xl gap-1 flex items-center">
-      <img src={logo} alt="logo" class="h-8" />
+    <a href="/home" class="text-2xl gap-1 flex items-center">
+      <img src={logo.src} alt="logo" class="h-8" />
       <span class="text-3xl font-bold">LCCT</span>
     </a>
   </div>
   <div class="relative">
-    <button class="md:hidden"><img src={hBIcon} alt="Menu" /></button>
+    <button class="md:hidden"><img src={hBIcon.src} alt="Menu" /></button>
 
     <div
       class="flex absolute right-0 flex-col md:flex-row md:static w-52 md:w-auto"
     >
-      <NavItem href="/academic-programs" text="Academics" />
-      <NavItem href="/events-activities" text="Events & activites" />
-      <NavItem href="/contacts" text="Contacts" />
-      <NavItem href="https://preregis.lcctanauan.edu.ph/" text="Preregis" />
+      <NavItem dropdown={false} href="/academic-programs" text="Academics" />
+      <NavItem
+        dropdown={false}
+        href="/events-activities"
+        text="Events & activites"
+      />
+      <NavItem dropdown={false} href="/contacts" text="Contacts" />
+      <NavItem
+        dropdown={false}
+        href="https://preregis.lcctanauan.edu.ph/"
+        text="Preregis"
+      />
     </div>
   </div>
 </nav>
