730465c9d763c672f9d52d9cd844e7dbb4dd3abf
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2023-04-12 07:21:48 +0800

    feat(components): :sparkles: add fly svelte component for fly animations

---
+++ src/components/Fly.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { inview } from "svelte-inview";
+  let visible: boolean = false;
+  import { fly } from "svelte/transition";
+  type Options = Parameters<typeof fly>[1];
+  export let options: Options = {
+    duration: 500,
+    y: -200,
+  };
+</script>
+
+<div
+  use:inview={{
+    threshold: 1,
+    rootMargin: "-20% 0px -30% 0px",
+  }}
+  on:inview_enter={(event) => {
+    visible = true;
+  }}
+>
+  {#if visible}
+    <div in:fly={options}>
+      <slot />
+    </div>
+  {/if}
+</div>
